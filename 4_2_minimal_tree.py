560204003308c02e423ad88a965c03629ca15111
TJ Tripp <tj.tripp@tpl.org>
2017-03-09 10:56:45 -0800

    Add notes on BST implementation.

--- 4_2_minimal_tree.py
+++ 4_2_minimal_tree.py
@@ -14,7 +14,10 @@ eliminate the need for self balancing?
 Doing the problem on paper shows that taking the mid point of the list as root.
 slicing the list at this mid point and taking the mid point of each subsequen slice
 recursively will generate a balanced binary search tree.
+Even and odd list lengths require a decision on which side of the tree should be deeper.
 
+The BST implementation in pythonds is maybe a little overkill for this problem.  After I finish it I need to think about how to trim it down to work just for these problem.
+In otherwords in an interview would I assume the BST was implemented and refer to the methods of the assumed implementation or would I implement one with the minimal features needed to finish the problem.
 '''
 
 
