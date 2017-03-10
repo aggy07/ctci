fc23fa5c0503941f59ccbbbefa4daf3545ca01b1
TJ Tripp <tj.tripp@tpl.org>
2017-03-10 13:40:04 -0800

    Add finished solution to 4_2_minimal_tree.py

--- 4_2_minimal_tree.py
+++ 4_2_minimal_tree.py
@@ -1,23 +1,49 @@
 # 4_2_minimal_tree - given a ascending sorted array of unigue ints create a binary seart tree with minimal height
-# Hints: #79, #73, #7 76 
+# Hints: #19, #73, #116 
 
 ''' 
 binary search tree means all left descendants <= n < all right descendents
 minimal height means the branches should be as balanced as possible
-implement a binary search tree class with add method
-then try testing starting with small lists of integers.
 to make a balanced tree we need to pick a root node that is going to 
 have an equal number of children on each branch.
 Does this require an AVL tree? Or will picking the correct root from the array
 eliminate the need for self balancing?
 
-Doing the problem on paper shows that taking the mid point of the list as root.
-slicing the list at this mid point and taking the mid point of each subsequen slice
+Doing the problem on paper shows that taking the mid point of the list as root gives same number of nodes on each side of tree
+slicing the list at this mid point and taking the mid point of each subsequent slice
 recursively will generate a balanced binary search tree.
-Even and odd list lengths require a decision on which side of the tree should be deeper.
+Doing the problem on paper I'm having trouble generalizing into a formula/function.  
+I was having trouble keeping track of my place in the sub array.  The solution used the start and end of each 
+slice to track it.
 
-The BST implementation in pythonds is maybe a little overkill for this problem.  After I finish it I need to think about how to trim it down to work just for these problem.
-In otherwords in an interview would I assume the BST was implemented and refer to the methods of the assumed implementation or would I implement one with the minimal features needed to finish the problem.
+The BST implementation in pythonds is overkill for this problem.  All I needed was a node class.
+
+Solution:
+https://github.com/careercup/CtCI-6th-Edition-Python/blob/master/Chapter4/42MinimalTree.py
+
 '''
+class Node:
+    def __init__(self,item):
+        self.right = None
+        self.left = None
+        self.val = item
+
+    def __str__(self):
+        return '('+str(self.left)+':L ' + "V:" + str(self.val) + " R:" + str(self.right)+')'
+
+def initiateArrayToBST(array):
+    return arrayToBST(array, 0, len(array)-1)
+
+def arrayToBST(array,start,end):
+    if start > end:
+        return ''
+    mid = ((start + end) // 2) 
+    root = Node(array[mid])
+    root.left = arrayToBST(array, start, mid -1)
+    root.right = arrayToBST(array, mid + 1, end)
+    return root
 
 
+if __name__ == '__main__':
+    testArray = [1,2,3,4,5,6,7]
+    print(initiateArrayToBST(testArray))
